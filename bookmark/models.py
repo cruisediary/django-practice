3870f52085f5d9330abc2d77b8c024a88b7e1527
cruisediary <cruzdiary@gmail.com>
2016-12-29 20:13:15 +0900

    chore: migrate bookmark model

--- bookmark/models.py
+++ bookmark/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 from __future__ import unicode_literals
+from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 # Create your models here.
 
