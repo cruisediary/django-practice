58df0fceeb5b1481d115ff45486012d52c7e1c32
cruisediary <cruzdiary@gmail.com>
2016-12-29 21:00:00 +0900

    feat(model): migrate bookmark model

--- bookmark/models.py
+++ bookmark/models.py
@@ -1,6 +1,8 @@
 from __future__ import unicode_literals
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
+
+
 # Create your models here.
 
 @python_2_unicode_compatible
@@ -9,4 +11,4 @@ class Bookmark(models.Model):
     url = models.URLField('url', unique=True)
 
     def __str__(self):
-        return self.title
+        return "%s %s" % (self.title, self.url)
