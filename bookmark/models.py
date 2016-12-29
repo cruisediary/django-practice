d43d786f6ed0a6f4979bfdff909a61de17a43af6
cruisediary <cruzdiary@gmail.com>
2016-12-29 20:06:22 +0900

    feat(model): add bookmark model

--- bookmark/models.py
+++ bookmark/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from __future__ import unicode_literals
+from django.utils.encoding import python_2_unicode_compatible
 # Create your models here.
+
+@python_2_unicode_compatible
+class Bookmark(models.Model):
+    title = models.CharField(max_length=100, blank=True, null=True)
+    url = models.URLField('url', unique=True)
+
+    def __str__(self):
+        return self.title
