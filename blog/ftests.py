1b121c8efbca4ba97811c2af206cbee037b94978
cruisediary <cruzdiary@gmail.com>
2016-12-31 17:11:06 +0900

    test(ft): add blog app functional test

---
+++ blog/ftests.py
@@ -0,0 +1,19 @@
+import time
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.common.keys import Keys
+
+
+class BlogTest(StaticLiveServerTestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.set_window_size(1024, 768)
+
+    def tearDown(self):
+        time.sleep(3)
+        self.browser.quit()
+
+    def test_blog_app(self):
+        self.browser.get('http://127.0.0.1:8000/blog/')
+
