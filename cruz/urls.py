1bb4213af1f009540c1fe181806d7714a1cf854b
cruisediary <cruzdiary@gmail.com>
2017-01-07 23:48:12 +0900

    feat: Add Home url

--- cruz/urls.py
+++ cruz/urls.py
@@ -17,8 +17,10 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from blog.urls import url
 from bookmark.urls import url
+from cruz.views import HomeView
 
 urlpatterns = [
+    url(r'^$', HomeView.as_view(), name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^bookmark/', include('bookmark.urls', namespace='bookmark')),
     url(r'^blog/', include('blog.urls', namespace='blog')),
